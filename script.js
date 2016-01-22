a704d477fa980236790aca8726c29167d22735ad
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-01-22 12:01:28 -0700

    number 2

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function letterCapitalize(inputString) {
-    var inputString = document.form1.instring;
-    var outputString = document.form1.outstring;
-    var letter = inputString.substr(0,1);
-    var str = inputString.toLowerCase();
-    outputString.value = letter.toUpperCase() + str.substr(1);
-    
+//function letterCapitalize(inputString) {
+//    var inputString = document.form1.instring;
+//    var outputString = document.form1.outstring;
+//    var letter = inputString.substr(0,1);
+//    var str = inputString.toLowerCase();
+//    outputString.value = letter.toUpperCase() + str.substr(1);
+//    
+//}
+
+
+function wordCount(inputString) {
+    var wordArray = inputString.trim().split(" ");
+    return wordArray.length;
 }
